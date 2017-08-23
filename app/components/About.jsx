b3a5481cde52425e18a514271adb62abd2ce29b3
Vineet Tyagi <Vineet.Tyagi@iontrading.com>
2017-08-23 11:00:24 +0530

    Styled About page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -2,7 +2,24 @@ var React = require('react');
 
 var About = (props) => {
     return(
-      <h3>About Component</h3>
+      <div>
+        <h1 className="text-center">About</h1>
+      <p>
+        This is a weather application built in react which gives you current temperature, in celcious, of any city you type.
+      </p>
+      <p>
+        Here are some of the tools I used:
+      </p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react">React</a> - This was the
+          Javascript framework used.
+        </li>
+        <li>
+          <a href="http://openWeatherMap.org">Open Weather Map</a> - I used Open Weather Map to search for weather data by city name.
+        </li>
+      </ul>
+      </div>
     );
   };
 
